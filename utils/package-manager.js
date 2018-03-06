4846b70bd07972a767e33500c2addfdc7fc9f6ce
Harshit Jain <hj.harshit007@gmail.com>
2018-03-07 00:10:12 +0530

    Fixes #12: npm dependencies install after init

---
+++ utils/package-manager.js
@@ -0,0 +1,19 @@
+const spawn = require("cross-spawn");
+
+/**
+ *
+ * Returns the name of package manager to use,
+ * preferring yarn over npm if available
+ *
+ * @returns {String} - The package manager name
+ */
+
+const getPackageManager = () => {
+	if (spawn.sync("yarn", [" --version"], { stdio: "ignore" }).error) {
+		return "npm";
+	}
+
+	return "yarn";
+};
+
+module.exports = getPackageManager;
