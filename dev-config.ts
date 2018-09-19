f33e71818c3e177f2fb97930b69c36040e82028c
Ajay Poshak <poshakajay@gmail.com>
2018-09-19 21:07:10 +0530

    Fix code indentation to pass TS tests

--- dev-config.ts
+++ dev-config.ts
@@ -1,34 +1,34 @@
 const getWebappWebpackPlugin = (config) => `new WebappWebpackPlugin({
-		logo: './${config.favPath}',
-		favicons: {
-			appName: '${config.manifestDetails.shortName}',
-			appDescription: '${config.manifestDetails.description}',
-			start_url: '${config.manifestDetails.startURL}',
-			theme_color: '${config.manifestDetails.themeColor}'
-		}
-	})`;
+	logo: './${config.favPath}',
+	favicons: {
+		appName: '${config.manifestDetails.shortName}',
+		appDescription: '${config.manifestDetails.description}',
+		start_url: '${config.manifestDetails.startURL}',
+		theme_color: '${config.manifestDetails.themeColor}'
+	}
+})`;
 
 const getSwDetails = () => `new GenerateSW({
-			clientsClaim: true,
-			skipWaiting: true,
-			runtimeCaching: [{
-				urlPattern: /\\/\$/,
-				handler: 'networkFirst',
-				options: {
-			  		cacheName: 'sw-app-index'
-				}
-			}]
-		})`;
+		clientsClaim: true,
+		skipWaiting: true,
+		runtimeCaching: [{
+			urlPattern: /\\/\$/,
+			handler: 'networkFirst',
+			options: {
+				  cacheName: 'sw-app-index'
+			}
+		}]
+	})`;
 
 export default function createDevConfig(config) {
- const plugins = [];
+  const plugins = [];
 
   if (config && config.serviceWorker) {
-   plugins.push(getSwDetails(), "new HtmlWebpackPlugin({filename:'index.html',template:'./templates/_index.html'})");
+    plugins.push(getSwDetails(), "new HtmlWebpackPlugin({filename:'index.html',template:'./templates/_index.html'})");
   }
   if (config && config.favPath) {
-   plugins.push(getWebappWebpackPlugin(config));
+    plugins.push(getWebappWebpackPlugin(config));
   }
 
- return { plugins };
+  return { plugins };
 }
