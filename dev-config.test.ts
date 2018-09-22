965819dc5a27345584ccfc30d0660c215c5937e2
Ajay Poshak <poshakajay@gmail.com>
2018-09-22 16:19:42 +0530

    fix(typescript): Convert tabs to spaces

--- dev-config.test.ts
+++ dev-config.test.ts
@@ -3,83 +3,83 @@ import createDevConfig from "./dev-config";
 
 const favPath = "test.js";
 const manifestDetails = {
- description: "index.html",
- hasManifest: false,
- name: "test",
- shortName: "test",
- startURL: "/",
- themeColor: "#ffffff",
+  description: "index.html",
+  hasManifest: false,
+  name: "test",
+  shortName: "test",
+  startURL: "/",
+  themeColor: "#ffffff",
 };
 const outputDir = "./dist";
 const swExpected = `new GenerateSW({
-			clientsClaim: true,
-			skipWaiting: true,
-			runtimeCaching: [{
-				urlPattern: /\\/\$/,
-				handler: 'networkFirst',
-				options: {
-			  		cacheName: 'sw-app-index'
-				}
-			}]
-		})`;
+  clientsClaim: true,
+  skipWaiting: true,
+  runtimeCaching: [{
+    urlPattern: /\\/\$/,
+    handler: 'networkFirst',
+    options: {
+        cacheName: 'sw-app-index'
+    }
+  }]
+})`;
 const htmlExpected = "new HtmlWebpackPlugin({filename:'index.html',template:'./templates/_index.html'})";
 const fpExpected = `new WebappWebpackPlugin({
-		logo: './test.js',
-		favicons: {
-			appName: 'test',
-			appDescription: 'index.html',
-			start_url: '/',
-			theme_color: '#ffffff'
-		}
-	})`;
+  logo: './test.js',
+  favicons: {
+    appName: 'test',
+    appDescription: 'index.html',
+    start_url: '/',
+    theme_color: '#ffffff'
+  }
+})`;
 
 test("create dev config to return when serviceworker is true", (t) => {
- const config = {
-  serviceWorker: true,
- };
- const { plugins } = createDevConfig(config);
- t.is(plugins.length, 2);
- t.is(plugins[0], swExpected);
- t.is(plugins[1], htmlExpected);
+  const config = {
+    serviceWorker: true,
+  };
+  const { plugins } = createDevConfig(config);
+  t.is(plugins.length, 2);
+  t.is(plugins[0], swExpected);
+  t.is(plugins[1], htmlExpected);
 });
 
 test("create dev config to return when only favPath is defined", (t) => {
- const config = {
-  favPath,
-  manifestDetails,
-  serviceWorker: false,
- };
- const { plugins } = createDevConfig(config);
- t.is(plugins.length, 1);
- t.is(plugins[0], fpExpected);
+  const config = {
+    favPath,
+    manifestDetails,
+    serviceWorker: false,
+  };
+  const { plugins } = createDevConfig(config);
+  t.is(plugins.length, 1);
+  t.is(plugins[0], fpExpected);
 });
 
 test("create dev config to return empty when both are sent", (t) => {
- const config = {
-  favPath,
-  manifestDetails,
-  serviceWorker: true,
- };
- const { plugins } = createDevConfig(config);
- t.is(plugins.length, 3);
- t.is(plugins[0], swExpected);
- t.is(plugins[1], htmlExpected);
- t.is(plugins[2], fpExpected);
+  const config = {
+    favPath,
+    manifestDetails,
+    serviceWorker: true,
+  };
+  const { plugins } = createDevConfig(config);
+  t.is(plugins.length, 3);
+  t.is(plugins[0], swExpected);
+  t.is(plugins[1], htmlExpected);
+  t.is(plugins[2], fpExpected);
 });
 // Removed Existing manifest testing.
 // tslint:disable-next-line:max-line-length
 test("create dev config when serviceWorker, favPath, manifestDetails, outputDir are sent and new manifest file is created", (t) => {
- const config = {
-  favPath,
-  manifestDetails,
-  outputDir,
-  serviceWorker: true,
-};
- const { plugins } = createDevConfig(config);
- t.is(plugins.length, 3);
- t.is(plugins[0], swExpected);
- t.is(plugins[1], htmlExpected);
- t.is(plugins[2], fpExpected);
+  const config = {
+    favPath,
+    manifestDetails,
+    outputDir,
+    serviceWorker: true,
+  };
+  const { plugins } = createDevConfig(config);
+  t.is(plugins.length, 3);
+  t.is(plugins[0], swExpected);
+  t.is(plugins[1], htmlExpected);
+  t.is(plugins[2], fpExpected);
 });
 
 test("create dev config to return empty when config is empty", (t) => t.is(createDevConfig({}).plugins.length, 0));
--- dev-config.ts
+++ dev-config.ts
@@ -1,24 +1,24 @@
 const getWebappWebpackPlugin = (config) => `new WebappWebpackPlugin({
-	logo: './${config.favPath}',
-	favicons: {
-		appName: '${config.manifestDetails.shortName}',
-		appDescription: '${config.manifestDetails.description}',
-		start_url: '${config.manifestDetails.startURL}',
-		theme_color: '${config.manifestDetails.themeColor}'
-	}
+  logo: './${config.favPath}',
+  favicons: {
+    appName: '${config.manifestDetails.shortName}',
+    appDescription: '${config.manifestDetails.description}',
+    start_url: '${config.manifestDetails.startURL}',
+    theme_color: '${config.manifestDetails.themeColor}'
+  }
 })`;
 
 const getSwDetails = () => `new GenerateSW({
-		clientsClaim: true,
-		skipWaiting: true,
-		runtimeCaching: [{
-			urlPattern: /\\/\$/,
-			handler: 'networkFirst',
-			options: {
-				  cacheName: 'sw-app-index'
-			}
-		}]
-	})`;
+  clientsClaim: true,
+  skipWaiting: true,
+  runtimeCaching: [{
+    urlPattern: /\\/\$/,
+    handler: 'networkFirst',
+    options: {
+        cacheName: 'sw-app-index'
+    }
+  }]
+})`;
 
 export default function createDevConfig(config) {
   const plugins = [];
