18bf4b8c42350e113e67f3c9eec56bc929dd7cde
Ajay Poshak <poshakajay@gmail.com>
2018-09-22 18:27:46 +0530

    Move questions to a separate file (#61)
    * feat(architecture): Move questions to separate file
    
    * feat(architecture): Move questions to a separate file
    
    * feat(architecture): Create a file for questions
    
    * feat(architecture): Bind value of this variable with validate method
    
    * feat(architecture): Remove arrow operator from object method
    
    * feat(chore): Simplified an nested condition
    
    * Fix code indentation to pass TS tests
    
    * feat(architecture): Add system generated files to gitignore
    
    * feat(architecture): Add typescript to questions
    
    * feat(architecture): Update editorconfig to support space instead of tabs
    
    * fix(typechecking): Fix trailing commas issue
    
    * fix(typescript): Convert tabs to spaces
    
    * feat(architecture):  Add static type checking
    
    * feat(architecture): Remove js file as it has been moved to TS

--- generator.js
+++ generator.js
@@ -1,9 +1,9 @@
-const path = require('path');
 const Generator = require('yeoman-generator');
 const { Confirm, Input } = require('@webpack-cli/webpack-scaffold');
 const createDevConfig = require('./dev-config');
 const getPackageManager = require('./utils/package-manager');
 const welcomeMessage = require('./utils/welcome');
+const questions = require('./questions');
 
 module.exports = class WebpackGenerator extends Generator {
 	constructor(args, opts) {
@@ -32,34 +32,14 @@ module.exports = class WebpackGenerator extends Generator {
 		let manifestDetails = {};
 		let favPath;
 		let outputDir;
-		const startUrlQuestion = {
-			default: () => "/",
-			message: "Enter startURL for your application: ",
-			name: "startURL",
-			type: "input"
-		};
 
 		this.devConfig.topScope = [
 			"const webpack = require('webpack')",
 			"const path = require('path')"
 		];
 
-		const entryQuestion = {
-			default: () => "'./index.js'",
-			message: 'Which file would be the first to enter the application?',
-			name: 'entryFile',
-			type: 'input',
-			validate: value => {
-				const pattern = /[^\s]*.js/i;
-				if (pattern.test(value)) {
-					return true;
-				} else {
-					return "Invalid path or file.";
-				}
-			}
-		};
-		welcomeMessage.default();
-		return this.prompt(entryQuestion)
+		welcomeMessage();
+		return this.prompt(questions.entryQuestion)
 			.then(entryAnswer => {
 				this.webpackOptions.entry = entryAnswer['entryFile'];
 
@@ -83,64 +63,7 @@ module.exports = class WebpackGenerator extends Generator {
 				if (answer['hasManifest']) {
 					return this.prompt([Input('manifestPath', 'Enter the path to your Manifest file')]);
 				} else {
-					const nameQuestion = {
-						default: () => process.cwd().split(path.sep)
-							.pop(),
-						message: "Let's create one. What is the name of your application?",
-						name: 'name',
-						type: 'input',
-						validate: value => {
-							if (value.length <= 45) {
-								return true;
-							} else {
-								return "Name is too long. Please enter a shorter name (less than 45 characters)";
-							}
-						}
-					};
-
-					const shortNameQuestion = {
-						default: () => process.cwd().split(path.sep)
-							.pop(),
-						message: "Enter a short name for your application",
-						name: 'shortName',
-						type: 'input',
-						validate: value => {
-							if (value.length <= 12) {
-								return true;
-							} else {
-								return "Short Name is too long. Please enter a shorter name (less than 12 characters)";
-							}
-						}
-					};
-
-					const descriptionQuestion = {
-						message: 'Enter description of you application: ',
-						name: 'description',
-						type: 'input'
-					};
-
-					const homePageQuestion = {
-						default: () => "index.html",
-						message: 'What is the name of the home page of your application?',
-						name: 'homePage',
-						type: 'input'
-					};
-
-					const themeColorQuestion = {
-						default: () => "#ffffff",
-						message: 'Please enter the theme color of your application.',
-						name: 'themeColor',
-						type: 'input',
-						validate: value => {
-							const pattern = /^#(?:[0-9a-fA-F]{3}){1,2}$/i;
-							if (pattern.test(value)) {
-								return true;
-							} else {
-								return "Invalid Hex color code. A valid color looks like #de54ef or #abc";
-							}
-						}
-					};
-
+					const { nameQuestion, shortNameQuestion, descriptionQuestion, homePageQuestion, themeColorQuestion, startUrlQuestion } = questions;
 					return this.prompt([nameQuestion, shortNameQuestion, descriptionQuestion, homePageQuestion, themeColorQuestion, startUrlQuestion]);
 				}
 			})
@@ -157,23 +80,9 @@ module.exports = class WebpackGenerator extends Generator {
 			})
 			.then(answer => {
 				if (answer['favicon'] === true) {
-					const faviconQuestion = {
-						message: 'Enter path to your logo (in .svg or .png): ',
-						name: 'favPath',
-						type: 'input',
-						validate: value => {
-							if (this.fs.exists(value)) {
-								if (value.endsWith(".png") || value.endsWith(".svg")) {
-									return true;
-								} else {
-									return "Favicon can be in .svg or .png only";
-								}
-							} else {
-								return "Given file doesn't exists.";
-							}
-						}
-					};
-					return this.prompt([faviconQuestion]);
+					// Binding the value of `this`
+					questions.faviconQuestion.validate = questions.faviconQuestion.validate.bind(this);
+					return this.prompt([questions.faviconQuestion]);
 				} else {
 					this.fs.copy(
 						this.templatePath('webpackIcon.png'),
@@ -191,14 +100,7 @@ module.exports = class WebpackGenerator extends Generator {
 					this.dependencies.push("webapp-webpack-plugin");
 				}
 
-				const outputDirQuestion = {
-					default: () => './dist',
-					message: 'Enter the path to directory where you would like to generate builds: ',
-					name: 'outputDir',
-					type: 'input'
-				};
-
-				return this.prompt([outputDirQuestion]);
+				return this.prompt([questions.outputDirQuestion]);
 			})
 			.then(answer => {
 				if (answer) {
@@ -228,11 +130,11 @@ module.exports = class WebpackGenerator extends Generator {
 	 */
 	installPlugins() {
 		const pkgManager = getPackageManager();
-		if(pkgManager==="yarn") {
+		if (pkgManager === "yarn") {
 			this.runInstall(pkgManager, this.dependencies, {
 				"dev": true
 			});
-		}else{
+		} else {
 			this.runInstall(pkgManager, this.dependencies, {
 				"save-dev": true
 			});
